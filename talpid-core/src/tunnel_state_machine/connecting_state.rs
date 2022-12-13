a1e95793ad524e5b7e7800845d20adc0a83f194a
David Lönnhager <david.l@mullvad.net>
2022-12-13 16:34:23 +0100

    Don't unregister tunnel IP in ST driver when reconnecting
    Similarly, if we enter the error state due to being offline, don't
    send an update
    
    Since the IP is likely to be identical after reconnecting, this should
    lower the number of IOCTLs to 0 from 2. There is a small risk that
    some other network interface will take over the previous tunnel IP and
    be incorrectly rebound to the default interface, though this should not
    be dangerous from a security standpoint, since it only applies to
    excluded processes

--- talpid-core/src/tunnel_state_machine/connecting_state.rs
+++ talpid-core/src/tunnel_state_machine/connecting_state.rs
@@ -544,18 +544,6 @@ impl TunnelState for ConnectingState {
                 ErrorState::enter(shared_values, ErrorStateCause::TunnelParameterError(err))
             }
             Ok(tunnel_parameters) => {
-                #[cfg(windows)]
-                if let Err(error) = shared_values.split_tunnel.set_tunnel_addresses(None) {
-                    log::error!(
-                        "{}",
-                        error.display_chain_with_msg(
-                            "Failed to reset addresses in split tunnel driver"
-                        )
-                    );
-
-                    return ErrorState::enter(shared_values, ErrorStateCause::SplitTunnelError);
-                }
-
                 if let Err(error) = Self::set_firewall_policy(
                     shared_values,
                     &tunnel_parameters,
